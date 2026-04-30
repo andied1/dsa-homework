923775ff124d36a1a8971f7bdb880c03e6ed4e8a
Andy Huynh <andyh22061@gmail.com>
2026-04-30 13:33:40 -0400

    hw7 files

--- week7/HW7/campus-taskboard/src/main/java/edu/brooklyn/cisc3130/taskboard/CampusTaskboardApplication.java
+++ week7/HW7/campus-taskboard/src/main/java/edu/brooklyn/cisc3130/taskboard/CampusTaskboardApplication.java
@@ -6,11 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CampusTaskboardApplication {
 
-/*	@SpringBootConfiguration - Marks this as a Spring configuration class
-	@EnableAutoConfiguration - Enables Spring Boot's auto-configuration
-	@ComponentScan - Scans for Spring components in the package
- */
-
 	public static void main(String[] args) {
 		SpringApplication.run(CampusTaskboardApplication.class, args);
 	}
